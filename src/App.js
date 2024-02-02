a411c9fc237a39d0997615c9c27014dc4af45b1e
rizky <rizkypurnama844@gmail.com>
2024-02-02 17:51:29 +0700

    fix app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './app.css';
 import './styles.css';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
 export default function ListingAd({ pic, title, address, description, psf_min, psf_max, project_type, year, ownership_type, subprice_label, availabilities_label, icon }) {
   const [showDescription, setShowDescription] = useState(false);
   const [anonymizeNumbers, setAnonymizeNumbers] = useState(true);
@@ -29,12 +28,12 @@ export default function ListingAd({ pic, title, address, description, psf_min, p
   return (
     <div className="App p-4">
       <div className='card shadow-2xl'>
-        <Carousel className='carousel-inner' showThumbs={false} showStatus={false} >
+        <Carousel className='shadow-inner' showThumbs={false}>
           <div>
-            <img className='w-full h-full' src={pic}  alt="Slide 1" />
+            <img src={pic} style={{ height: '272px', width: '544px' }} alt="Slide 1" />
           </div>
           <div>
-            <img className='w-full h-full 'src={pic}  alt="Slide 2" />
+            <img src={pic} style={{ height: '272px', width: '544px' }} alt="Slide 2" />
           </div>
         </Carousel>
           <div className='wrapper flex flex-row p-4'>
@@ -54,22 +53,22 @@ export default function ListingAd({ pic, title, address, description, psf_min, p
                </div>
               </div>   
             <div className=' col-2 text-end flex flex-col'>
-                <div className='avenir-demibold f-color2' style={{fontSize: '19px'}}>${psf_min} - ${psf_max} psf</div>
+                <div className='avenir-demibold' style={{fontSize: '19px'}}>${psf_min} - ${psf_max} psf</div>
                 <div className='avenir-regular f-14 f-color1 text-1'>{subprice_label}</div>
             </div>
           </div>
           <div className='pb-4 pr-4 pl-4 text-end avenir-demibold f-16 f-color3' >
-            <button 
-                className='focus:outline-none
-                          hover:text-orange-300'
-                onClick={handleSeeDescriptionClick}>
-                     See description
-              </button>
-              {showDescription && (
-                <div>
-                  {getDescriptionContent()}
-                </div>
-              )}
+          <button 
+              className='focus:outline-none
+                        hover:text-orange-300'
+              onClick={handleSeeDescriptionClick}
+>            See description
+            </button>
+            {showDescription && (
+              <div>
+                {getDescriptionContent()}
+              </div>
+            )}
           </div>
         </div>
       </div>
